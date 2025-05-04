91a86c227043cbbff46e0b5ae47b29c004824e4b
John Imeobong <imeobongjohn38@gmail.com>
2025-05-04 02:25:08 +0100

    Chapter 2

--- Chapter 2/ascending_order.py
+++ Chapter 2/ascending_order.py
@@ -6,7 +6,6 @@ a = float(input('Enter first decimal number: '))
 b = float(input('Enter second decimal number: '))
 c = float(input('Enter third decimal number: '))
 
-if a > b > c:
-	print(a, ' is the largest')
-else if b > a > c:
-	print(b, '')
+numbers = [a, b, c]
+numbers.sort()
+print(numbers)
